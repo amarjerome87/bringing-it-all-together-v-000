3f50ff9cb3fe702b07eceec22639bb6067994520
Amar Chatman <amarjerome@gmail.com>
2019-10-04 19:23:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,6 +72,10 @@ def self.find_or_create_by(name:, breed:)
 end
 
 def self.find_by_name(name)
+  sql = "SELECT * FROM dogs WHERE name = ?"
+     result = DB[:conn].execute(sql, name)[1]
+     #binding.pry
+     Dog.new(id:result[0], name:result[1], breed:result[2])
 end
 
 
